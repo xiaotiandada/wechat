fedb00c32b51a6fbe641f40ddbb4c750b6a11097
xiaotiandada <952822399@qq.com>
2018-02-10 15:03:27 +0800

    wechat

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+
+  // 获取微信服务器GET请求的参数 signature,timestamp,nonce,echostr
+    var signature = req.query.signature, // 微信加密签名
+        timestamp = req.query.timestamp, // 时间戳
+        nonce = req.query.nonce, // 随机数
+        echostr = req.query.echostr; // 随机字符串
+
+    // 奖token, timestamp, nonce 三个参数进行字典序排序
+    var array = [config.token,timestamp,nonce];
+    array.sort()
+
+    //  将三个参数字符串拼接成一个字符串进行sha1加密
+    var tempStr = array.join('');
+    const hashCode = crypto.createHash('sha1'); //  创建加密类型
+    var resultCode = hashCode.update(tempStr,'utf8').digest('hex'); // 对传入的字符串进行加密
+
+    // 开发者获得加密后的字符串可与signature对比, 标识请求来源于微信
+    if(resultCode === signature){
+      res.send(echostr);
+    } else {
+      res.send('mismatch');
+    }
+});
+
+module.exports = router;
